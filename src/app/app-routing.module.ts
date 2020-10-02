2c118b24e77a2014761bb62c8726b24bbfcd65b7
Spekks <hectoroc88@gmail.com>
2020-10-02 10:23:20 +0200

    Viernes 2/10. Creacion Angular proj

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { NoticiasComponent } from './pages/noticias/noticias.component';
+import { SugerenciasComponent } from './pages/sugerencias/sugerencias.component';
+
+const routes: Routes = [
+
+  {
+    path: 'inicio',
+    component: InicioComponent,
+  },
+
+  {
+    path: 'noticias',
+    component: NoticiasComponent,
+  },
+
+  {
+    path: 'sugerencias',
+    component: SugerenciasComponent,
+  },
+
+  {
+    path: '**',
+    redirectTo: 'inicio',
+  }
+
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes),
+  ],
+  exports: [
+    RouterModule,
+  ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { NoticiasComponent } from './pages/noticias/noticias.component';
+import { SugerenciasComponent } from './pages/sugerencias/sugerencias.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    NoticiasComponent,
+    SugerenciasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
